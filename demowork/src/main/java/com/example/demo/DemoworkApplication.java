c2fd639fa145b89ebfc5ee6ea2e4713c25fead17
Jane Linux <283199611@qq.com>
2018-12-10 06:10:42 -0500

    add some test git file

--- demowork/src/main/java/com/example/demo/DemoworkApplication.java
+++ demowork/src/main/java/com/example/demo/DemoworkApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScans;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.demo.*"})
 public class DemoworkApplication {
-
+//nice to mee you  linux.
 	public static void main(String[] args) {
 		SpringApplication.run(DemoworkApplication.class, args);
 	}
